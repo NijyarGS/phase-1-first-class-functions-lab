d74f56ad8f2e7f9bb23f6b3e859aac40166a59b7
Nijyar Ghazi <nijyar360@gmail.com>
2022-08-13 02:33:49 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//["james","sam","smith","harry","walter"]
+
+function createFareMultiplier(int) {
+return (mul) => {return int*mul}
+}
+
+let fareDoubler = (int) => createFareMultiplier(int)(2)
+let fareTripler = (int) => createFareMultiplier(int)(3)
+
+function selectDifferentDrivers(drivers,arg) {
+    return arg(drivers)
+
+}
